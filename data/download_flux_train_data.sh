9c51e19cdb2b7b2cf73db8e0baf78ecd76d1eecd
free001style <iadrobyshevskiy@yandex.ru>
2026-03-12 20:16:11 +0300

    add scripts to download data

---
+++ data/download_flux_train_data.sh
@@ -0,0 +1,3 @@
+wget -c https://storage.yandexcloud.net/yandex-research/flux_train_data.tar
+tar -xf flux_train_data.tar -C data/
+rm flux_train_data.tar
---
+++ data/download_sd35_medium_train_data.sh
@@ -0,0 +1,3 @@
+wget -c https://storage.yandexcloud.net/yandex-research/sd35_medium_train_data.tar
+tar -xf sd35_medium_train_data.tar -C data/
+rm sd35_medium_train_data.tar
